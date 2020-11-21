89d3eb6d26224385e95afe6d5b66b5c84e671544
cym <646598890@qq.com>
2020-11-21 19:12:51 +0800

    获取外网ip

--- python/获取外网ip.py
+++ python/获取外网ip.py
@@ -6,8 +6,9 @@ def getIP():
     headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36"
     response = requests.get('http://www.baidu.com/s?wd=ip',headers=headers)
     html = response.text
-    reGET = re.compile('fk="(.*?)"').findall(html)
-    for i in reGET:
-        print('外网地址： %s'%i)
+    ret = re.search(r'<span class="c-gap-right">本机IP:&nbsp;(.*?)</span>', html)
+    if ret:
+        print(ret.group(1))
+
 
 getIP()
