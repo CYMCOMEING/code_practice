f9d302e1519b0d2449669921e0ac3e0531078031
cym <646598890@qq.com>
2020-11-24 01:22:36 +0800

    图片数据库优化

--- python/图片对比/图片数据管理.py
+++ python/图片对比/图片数据管理.py
@@ -1,23 +1,40 @@
 import sqlite3
 import os
 
+
 class picSql():
     """
     建议单例模式
     """
+
     def __init__(self):
         self.filename = "picSql.db"
+        self.db = None
+        self.c = None
+
+    def open(self):
         self.db = sqlite3.connect(self.filename)
         self.c = self.db.cursor()
         self.create_table()
+        return self
+
+    def close(self):
+        """
+        关闭数据库
+        """
+        self.c.close()
+        self.db.close()
+        self.db = None
+        self.c = None
 
     def create_table(self):
-        #判断表头是否存在
+        # 判断表头是否存在
         try:
-            self.c.execute("create table if not exists PicData (id int primary key, path text not null,source text,cosin  text,similar  text,hash text);")
+            self.c.execute(
+                "create table if not exists PicData (id int primary key, path text not null,source text,cosin  text,"
+                "similar  text,hash text);")
         except Exception as e:
             print("create PicData table fail." + str(e))
-
 
     def add_pic(self, path):
         try:
@@ -35,8 +52,8 @@ class picSql():
             keys += i + "=?,"
         keys = keys[:-1]
         try:
-            #"update PicData set cosin=?,similar=?,hash=? where path=?;"
-            sql = "update PicData set "+ keys +" where path='"+path+"';"
+            # "update PicData set cosin=?,similar=?,hash=? where path=?;"
+            sql = "update PicData set " + keys + " where path='" + path + "';"
             self.c.execute(sql, value)
             count = self.db.total_changes
             self.db.commit()
@@ -57,8 +74,8 @@ class picSql():
 
     def search_one(self, path, key):
         try:
-            #"select * from  PicData where path=?"
-            self.c.execute("select "+key+" from  PicData where path=:path", {"path":path})
+            # "select * from  PicData where path=?"
+            self.c.execute("select " + key + " from  PicData where path=:path", {"path": path})
         except Exception as e:
             print("search_one fail." + str(e))
             return None
@@ -66,39 +83,40 @@ class picSql():
 
     def search_all(self, key):
         try:
-            #"select * from PicData"
-            self.c.execute("select "+key+" from  PicData")
+            # "select * from PicData"
+            self.c.execute("select " + key + " from  PicData")
         except Exception as e:
             print("search_all fail." + str(e))
             return None
         return self.c.fetchall()
-        
-    def close(self):
-        """
-        关闭数据库
-        """
-        self.c.close()
-        self.db.close()
+
+    # 支持
+    def __enter__(self):
+        return self.open()
+
+    def __exit__(self, type, value, trace):
+        print(type, value, trace)
+        self.close()
+
 
 if __name__ == "__main__":
-    ps = picSql()
+    ps = picSql().open()
     ps.add_pic("D:\\dddd.jpg")
     ps.add_pic("D:\\hhhh.jpg")
     ps.add_pic("D:\\yyyy.jpg")
     print(ps.search_all("*"), "\n")
     ps.modify_pic("D:\\hhhh.jpg", ("cosin",), ("SDFAFHSJLDF",))
-    
-    ps.modify_pic("D:\\yyyy.jpg", ("cosin","similar","hash"), ("SDFALNAGDS","SFDREGTJYTJ","LSDHGDKLGJU"))
+
+    ps.modify_pic("D:\\yyyy.jpg", ("cosin", "similar", "hash"), ("SDFALNAGDS", "SFDREGTJYTJ", "LSDHGDKLGJU"))
 
     print(ps.search_all("*"), "\n")
-    
+
     ps.del_pic("D:\\hhhh.jpg")
     print(ps.search_all("*"), "\n")
-    
+
     print(ps.search_all("path"), "\n")
     print(ps.search_all("path, hash"), "\n")
-    
-    print(ps.search_one("D:\\yyyy.jpg","path, similar, hash"), "\n")
-    
-    ps.close()
 
+    print(ps.search_one("D:\\yyyy.jpg", "path, similar, hash"), "\n")
+
+    ps.close()
