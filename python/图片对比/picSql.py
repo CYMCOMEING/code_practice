54f72f4786502fe5ff6f4786c36e6d00120a0c45
CYMCOMING <646598890@qq.com>
2020-11-24 17:02:49 +0800

    增加对比数据库的comperDatr表功能

--- python/图片对比/picSql.py
+++ python/图片对比/picSql.py
@@ -9,6 +9,7 @@ CompareData
 id1, id2, cosin, similar, hash
 """
 
+
 class picSql():
     def __init__(self):
         self.filename = "picSql.db"
@@ -80,7 +81,8 @@ class picSql():
     def search_one(self, path, key):
         try:
             # "select * from  PicData where path=?"
-            self.c.execute("select " + key + " from  PicData where path=:path", {"path": path})
+            self.c.execute("select " + key +
+                           " from  PicData where path=:path", {"path": path})
         except Exception as e:
             print("search_one fail." + str(e))
             return None
@@ -95,7 +97,81 @@ class picSql():
             return None
         return self.c.fetchall()
 
-    # 支持
+    def add_comp(self, **kwargs):
+        keys = ""
+        values = []
+        for kye in kwargs:
+            keys += kye + ","
+            values.append(kwargs[kye])
+        keys = keys[:-1]
+        try:
+            sql = "insert into CompareData ({}) values ({});".format(
+                keys, ','.join(('?'*len(values))))
+            self.c.execute(sql, values)
+            count = self.db.total_changes
+            self.db.commit()
+        except Exception as e:
+            print("add_comp fail." + str(e))
+            return False
+        return count > 0
+
+    def modify_comp(self, id1, id2, **kwargs):
+        keys = ''
+        value = []
+        for key in kwargs:
+            keys += key + "=?,"
+            value.append(kwargs[key])
+        keys = keys[:-1]
+        try:
+            sql = "update CompareData set " + keys + \
+                " where (id1=? and id2=?) or (id1=? and id2=?);"
+            self.c.execute(sql, value+[id1, id2, id2, id1])
+            count = self.db.total_changes
+            self.db.commit()
+        except Exception as e:
+            print("modify_comp fail. \n" + sql + str(e))
+            return False
+        return count > 0
+
+    def search_comp_id(self, id, key):
+        try:
+            self.c.execute("select " + key +
+                           " from  CompareData where id1=? or id2=?", [id, id])
+        except Exception as e:
+            print("search_comp_id fail." + str(e))
+            return None
+        return self.c.fetchall()
+
+    def search_comp_ids(self, id1, id2, key):
+        try:
+            self.c.execute("select " + key +
+                           " from  CompareData where (id1=:id1 and id2=:id2) or (id1=:id2 and id2=:id1);", {'id1': id1, 'id2': id2})
+        except Exception as e:
+            print("search_comp_ids fail." + str(e))
+            return None
+        return self.c.fetchall()
+
+    def search_comp_all(self, key):
+        try:
+            self.c.execute("select " + key +
+                           " from  CompareData ;")
+        except Exception as e:
+            print("search_comp_all fail." + str(e))
+            return None
+        return self.c.fetchall()
+
+    def del_comp(self, id1, id2):
+        try:
+            self.c.execute("delete from CompareData where (id1=:id1 and id2=:id2) or (id1=:id2 and id2=:id1);", {
+                           'id1': id1, 'id2': id2})
+            count = self.db.total_changes
+            self.db.commit()
+        except Exception as e:
+            print("del_comp fail." + str(e))
+            return False
+        return count > 0
+
+    # 支持with
     def __enter__(self):
         return self.open()
 
@@ -106,22 +182,34 @@ class picSql():
 
 if __name__ == "__main__":
     ps = picSql().open()
-    ps.add_pic("D:\\dddd.jpg")
-    ps.add_pic("D:\\hhhh.jpg")
-    ps.add_pic("D:\\yyyy.jpg")
-    print(ps.search_all("*"), "\n")
-    ps.modify_pic("D:\\hhhh.jpg", ("cosin",), ("SDFAFHSJLDF",))
+    # ps.add_pic("D:\\dddd.jpg")
+    # ps.add_pic("D:\\hhhh.jpg")
+    # ps.add_pic("D:\\yyyy.jpg")
+    # print(ps.search_all("*"), "\n")
+    # ps.modify_pic("D:\\hhhh.jpg", ("cosin",), ("SDFAFHSJLDF",))
 
-    ps.modify_pic("D:\\yyyy.jpg", ("cosin", "similar", "hash"), ("SDFALNAGDS", "SFDREGTJYTJ", "LSDHGDKLGJU"))
+    # ps.modify_pic("D:\\yyyy.jpg", ("cosin", "similar", "hash"), ("SDFALNAGDS", "SFDREGTJYTJ", "LSDHGDKLGJU"))
 
-    print(ps.search_all("*"), "\n")
+    # print(ps.search_all("*"), "\n")
 
-    ps.del_pic("D:\\hhhh.jpg")
-    print(ps.search_all("*"), "\n")
+    # ps.del_pic("D:\\hhhh.jpg")
+    # print(ps.search_all("*"), "\n")
 
-    print(ps.search_all("path"), "\n")
-    print(ps.search_all("path, hash"), "\n")
+    # print(ps.search_all("path"), "\n")
+    # print(ps.search_all("path, hash"), "\n")
 
-    print(ps.search_one("D:\\yyyy.jpg", "path, similar, hash"), "\n")
+    # print(ps.search_one("D:\\yyyy.jpg", "path, similar, hash"), "\n")
+############
+    ps.add_comp(id1=3, id2=4, cosin="sadff", similar="sdffsd", hash="sdfasf")
+    ps.add_comp(id1=6, id2=4, cosin="sadff", similar="sdffsd", hash="sdfasf")
+    ps.add_comp(id1=6, id2=8, cosin="sadff", similar="sdffsd", hash="sdfasf")
 
+    print(*ps.search_comp_id(4, "*"))
+    print(*ps.search_comp_id(6, "id1,id2,hash"))
+
+    print(*ps.search_comp_ids(4, 3, "*"))
+
+    ps.modify_comp(4, 3, similar="dfgs")
+    ps.del_comp(8, 6)
+    print(*ps.search_comp_all('*'))
     ps.close()
