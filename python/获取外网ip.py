dd977e0556816d15f0c160214759003049e3db14
CYMCOMING <646598890@qq.com>
2021-03-29 16:19:48 +0800

    ok

--- python/获取外网ip.py
+++ python/获取外网ip.py
@@ -4,7 +4,7 @@ import re
 def getIP():
     headers = {}
     headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36"
-    response = requests.get('http://www.baidu.com/s?wd=ip',headers=headers)
+    response = requests.get('https://www.baidu.com/s?wd=ip',headers=headers)
     html = response.text
     ret = re.search(r'<span class="c-gap-right">本机IP:&nbsp;(.*?)</span>', html)
     if ret:
